b5ad4d83f234cd71e8f30cba0a6c937fb4ccc487
Hosein <asadolahi.hosein226@gmail.com>
2024-08-24 22:11:22 +0330

    add article service

---
+++ src/main/java/com/github/hoseinasadolahi/bloggingapp/services/ArticleService.java
@@ -0,0 +1,26 @@
+package com.github.hoseinasadolahi.bloggingapp.services;
+
+import com.github.hoseinasadolahi.bloggingapp.model.Article;
+import com.github.hoseinasadolahi.bloggingapp.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    public Page<Article> findAllSorted(String sortBy, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        if (sortBy.equals("likes")) {
+            return articleRepository.findAllSortedByLikes(pageable);
+        }
+        return articleRepository.findAllByOrderByPublishedDateDesc(pageable);
+    }
+}
