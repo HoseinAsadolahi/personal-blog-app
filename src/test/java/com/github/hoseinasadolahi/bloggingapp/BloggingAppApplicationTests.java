0447bfb9e8b6ae00956a339153051dcdbde4f4a1
Hosein <asadolahi.hosein226@gmail.com>
2024-08-24 17:55:02 +0330

    init

---
+++ src/test/java/com/github/hoseinasadolahi/bloggingapp/BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hoseinasadolahi.bloggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
